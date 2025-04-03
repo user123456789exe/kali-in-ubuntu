ab52575c242a8d6b722663913deb70523685e9bb
user123456789exe <user123456879ntm@gmail.com>
2025-04-03 09:40:53 +0200

    Update kali-tools.sh

--- kali-tools.sh
+++ kali-tools.sh
@@ -1,8 +1,1 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt-get install -y wget curl gnupg2 lsb-release
-echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list.d/kali.list
-git clone https://github.com/yourusername/kali-tools-ubuntu-installer.git
-cd kali-tools-ubuntu-installer
-chmod +x install_kali_tools.sh
-sudo ./install_kali_tools.sh
-sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y && sudo apt-get install -y wget curl gnupg2 lsb-release && echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list.d/kali.list && git clone https://github.com/yourusername/kali-tools-ubuntu-installer.git && cd kali-tools-ubuntu-installer && chmod +x install_kali_tools.sh && sudo ./install_kali_tools.sh && sudo apt update && sudo apt upgrade -y
